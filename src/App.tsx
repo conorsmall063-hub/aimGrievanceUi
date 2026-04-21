0febbe77c84d183ae0f6ad1e214974e55ecd37cd
conor small <conorsmall063@gmail.com>
2026-04-22 06:59:05 +1200

    use translation file

--- src/App.tsx
+++ src/App.tsx
@@ -6,59 +6,14 @@ import { useState } from "react"
 import i18n from "i18next"
 import { useTranslation, initReactI18next } from "react-i18next";
 
+import enTranslation from './locales/en.json';
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources: {
       en: {
-        translation: {
-          chosenLanguage: 'Which is your prefered language?',
-          one: 'What would you like to report?',
-          two: '(Multi-select: Wage Theft, Excessive Work Hours, etc.)',
-          three: 'Please describe what is happening.',
-          four: 'What is different to what you were expecting?',
-          five: 'Has anyone taken your passport, threatened you, or stopped you from leaving your job?',
-          six: '(Examples: They keep your documents, don’t pay you, or scare you into staying)',
-          seven: 'Are you, or someone else, being forced to live in unsafe or overcrowded housing?',
-          eight: '(Yes / No / Not sure - add details if known)',
-          nine: 'Are there any children involved?',
-          ten: '(Yes / No / Not sure - add details if known)',
-
-          section2: ' Who Is Affected?',
-          one2: 'Who is being exploited?',
-          two2: '(e.g. yourself, someone you know, a group)',
-          three2: 'How many people are experiencing this?',
-          four2: 'How do you know about this situation?',
-          five2: '(e.g. I saw it, I heard it, someone told me, I experienced it)',
-
-          section3: 'Who’s Responsible',
-          one3: 'Do you know who is responsible?',
-          two3: '(e.g. name of employer, agent, landlord — include any details you know)',
-          three3: 'What is the name of the business or organisation involved (if any)?',
-
-          section4: 'Where and When',
-          one4: 'Where is this happening?',
-          two4: '(Address, city or region if known)',
-          three4: 'what kind of work is involved',
-          four4: 'When did this occur, and is it still happening?',
-          five4: '(Ongoing / Happened in the past)',
-
-          section5: 'Additional Info (Optional but Helpful)',
-          one5: 'Who else might know about this?',
-          two5: 'Has this been reported before?',
-          three5: '(Yes / No / Not sure -  if yes, to who?)',
-          four5: 'Would you like to upload any supporting files or photos?',
-          five5: 'Is there anything else we should know to help us understand or respond to this situation?',
-
-          section6: 'Privacy and Consent',
-          one6: 'Do you wish to remain anonymous?',
-          two6: '(Yes / No — We won’t collect any contact information unless you choose to share it. Providing contact details is optional, but it can help us get in touch to better understand your situation or connect you with support.)',
-          three6: 'CAPTCHA / Bot Check',
-          four6: '(Standard reCAPTCHA)',
-        
-        },
-        
+        translation: enTranslation
       },
       fr: {
         translation: {
@@ -88,9 +43,9 @@ function App() {
           <div className="flex gap-2 pb-4">
           <Button onClick = {() => i18n.changeLanguage('en')}>English</Button>
           <Button onClick= {() => i18n.changeLanguage('ta')}>Tagalog</Button>
-          <Button onClick= {() => i18n.changeLanguage('hi')}>हिंदी </Button>
-          <Button onClick= {() => i18n.changeLanguage('ma')}>普通话</Button>
-          <Button onClick= {() => i18n.changeLanguage('to')}>lea faka-Tonga</Button>
+          <Button onClick= {() => i18n.changeLanguage('ta')}>हिंदी </Button>
+          <Button onClick= {() => i18n.changeLanguage('ta')}>普通话</Button>
+          <Button onClick= {() => i18n.changeLanguage('ta')}>lea faka-Tonga</Button>
           </div>
           <div className="grid w-full gap-2 py-2">
             <FieldDescription>1. {t('one')}</FieldDescription>
@@ -129,23 +84,23 @@ function App() {
               placeholder={t('ten')} />
           </div>
 
-          <FieldLabel>{t('section2')}</FieldLabel>
+          <FieldLabel>{t('sectionTwo.Header')}</FieldLabel>
           <div className="grid w-full gap-2 py-2">
-            <FieldDescription>7. {t('one2')}</FieldDescription>
+            <FieldDescription>7. {t('sectionTwo.questionOne')}</FieldDescription>
             <Textarea 
               onChange={text => setText(text.target.value) }
-              placeholder={t('two2')} />
+              placeholder={t('sectionTwo.promptOne')} />
           </div>
           <div className="grid w-full gap-2 py-2">
-            <FieldDescription>8. {t('three2')}</FieldDescription>
+            <FieldDescription>8. {t('sectionTwo.questionTwo')}</FieldDescription>
             <Textarea 
               onChange={text => setText(text.target.value) } />
           </div>
           <div className="grid w-full gap-2 py-2">
-            <FieldDescription>9. {t('four2')}</FieldDescription>
+            <FieldDescription>9. {t('sectionTwo.questionThree')}</FieldDescription>
             <Textarea 
               onChange={text => setText(text.target.value) }
-              placeholder={t('five2')} />
+              placeholder={t('sectionTwo.promptThree')} />
           </div>
 
 
