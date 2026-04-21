5a8c511e64a09a672c62af96a41bfd74962b60d9
conor small <conorsmall063@gmail.com>
2026-04-21 12:02:22 +1200

    fix UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css'
+import { Button } from "./components/ui/button.tsx"
 
 function App() {
 
   return (
     <>
       <section id="center">
+        <div>
+          <Button> Click me</Button>
+        </div>
         <div>
           <h1>Grevence</h1>
         </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
+import path from "path"
 import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite"
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
@@ -6,6 +8,12 @@ import babel from '@rolldown/plugin-babel'
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
